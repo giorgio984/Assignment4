6690a4bb95db2bcbf3399a694143f9735764a3cf
Giorgio <ws_12@jogroup.eu>
2021-01-08 22:39:53 +0100

    Initial Commit

---
+++ Assignment4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment4
+//
+//  Created by MacBook Giorgio on 08/01/2021.
+//  Copyright © 2021 GM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
